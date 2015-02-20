b8603825a2269d44de127a0967294b471291e6ca
tctx <tommy.campbell@ymail.com>
2015-02-20 00:25:22 -0600

    Added ERB concatenation example

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,9 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+x1 = "rad"
+x2 = "super " + x1
+template2 = ERB.new "This is a <%= x2.upcase %> language"
+puts template2.result(binding)
+
