c0083260322cd41cb6e3c614f0b8696fbccbc218
tctx <tommy.campbell@ymail.com>
2015-02-20 00:29:13 -0600

    Changed concatenation wording

--- erb_example.rb
+++ erb_example.rb
@@ -4,7 +4,7 @@ x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
 
-x1 = "rad"
+x1 = "cool"
 x2 = "super " + x1
 template2 = ERB.new "This is a <%= x2.upcase %> language"
 puts template2.result(binding)
