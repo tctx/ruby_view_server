f68fcf514c350a71d691ec6327548c028910a2ef
tctx <tommy.campbell@ymail.com>
2015-03-02 16:16:49 -0600

    Made 2 changes to erb_example.rb

--- erb_example.rb
+++ erb_example.rb
@@ -1,11 +1,11 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new "The value of x is equal to <%= x %>"
 puts template.result(binding)
 
-x1 = "cool"
+x1 = "awesome"
 x2 = "super " + x1
-template2 = ERB.new "This is a <%= x2.upcase %> language"
+template2 = ERB.new "This is a <%= x2.downcase %> language"
 puts template2.result(binding)
 
